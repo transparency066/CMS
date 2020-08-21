ac921a34127028d929a5a078b4c88fb4a42c6cb5
tongjinyx <43092694+tongjinyx@users.noreply.github.com>
2020-08-21 16:05:34 +0800

    Add files via upload
    评论和反馈的model

---
+++ MovieModel/Complaint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieModel
+{
+    class Complaint
+    {
+        public DateTime ComplaintTime { get; set; }
+        public string userID { get; set; }
+        public string ComplaintContent { get; set; }
+        public int isReply { get; set; }
+    }
+}
