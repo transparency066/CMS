b39d5561fb88cd6a8698833a0072c77a1e380b85
tongjinyx <43092694+tongjinyx@users.noreply.github.com>
2020-08-21 16:08:21 +0800

    Add files via upload
    反馈部分控制器

---
+++ Controllers/MovieComplaintController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieBusinessLogic;
+using MovieModel;
+
+namespace MovieWeb.Controllers
+{
+    public class MovieComplaintController : Controller
+    {
+        // GET: MovieComplaint
+        Manager manager = new Manager();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection txt)
+        {
+            string content = txt["Complaint"];
+            DateTime time = DateTime.Now;
+            int reply = 0;
+            if (Session["uid"] == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                string uid = System.Web.HttpContext.Current.Session["uid"].ToString();
+                if (manager.Complain(time, uid, content, reply) > 0)
+                {
+                    Response.Write("提交成功,谢谢您的反馈！");
+                }
+                else
+                {
+                    Response.Write("提交失败！");
+
+                }
+
+                return View();
+            }
+        }
+    }
+}
