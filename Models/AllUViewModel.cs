cf028fde548e893acd007215961537ff0c26163d
bzl337 <51352888+bzl337@users.noreply.github.com>
2020-08-24 21:27:51 +0800

    Add files via upload

---
+++ Models/AllUViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace 影院管理系统.Models
+{
+    public class AllUViewModel
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int Sex { get; set; }
+    }
+}
