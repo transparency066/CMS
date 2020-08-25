a3abb8173c3579a7bb62c6bea1dc53b66f13a09a
cxy592394546 <cxy592394546@gmail.com>
2020-08-24 20:12:32 -0700

    Add files via upload

---
+++ MovieBusinessLogic/Manager_cxy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieModel;
+using MovieRepository.MySQL;
+
+namespace MovieBusinessLogic
+{
+    public class Manager
+    {
+        private MovieRepository.MySQL.ComplaintRepository repository1 = new MovieRepository.MySQL.ComplaintRepository();
+       public List<UComplaint> UserComplaint()
+        {        
+            return repository1.UntreatedComplaint();
+        }
+
+        private MovieRepository.MySQL.ReplyRepository repository2 = new MovieRepository.MySQL.ReplyRepository();
+        public int SendMess(MessageSend messageSend)
+        {
+            int flag = repository2.SendMessage(messageSend);
+            return flag;
+        }
+    }
+}
