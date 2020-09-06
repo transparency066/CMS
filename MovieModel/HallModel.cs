1c8f30341fbd27ab72980dfae8c5d105fe8ec741
only976 <42378887+only976@users.noreply.github.com>
2020-09-06 22:30:18 +0800

    Add files via upload

---
+++ MovieModel/HallModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieModel
+{
+    public class HallModel
+    {
+        public string screening_id { set; get; }
+        public string hall_id { set; get; }
+        public int total { set; get; }
+        public int left { set; get; }
+        
+    }
+}
