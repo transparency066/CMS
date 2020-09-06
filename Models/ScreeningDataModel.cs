f716cb1f0050af813ecf78b9f12bb3c461b04abc
only976 <42378887+only976@users.noreply.github.com>
2020-09-06 22:38:13 +0800

    Add files via upload

---
+++ Models/ScreeningDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Screening.Models
+{
+    public class ScreeningDataModel
+    {
+       
+        public string screening_id { set; get; }
+        public string film_id { set; get; }
+        public DateTime start_time { set; get; }
+        public string hall_id { set; get; }
+    }
+    public class ScreeningList
+    {
+        public List<ScreeningDataModel> ScreeningDataList { set; get; }
+    }
+}
---
+++ Models/SeatsListModel.cs
@@ -0,0 +1,15 @@
+﻿using MovieModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Screening.Models
+{
+    public class SeatsListModel
+    {
+       
+        
+        public List<SeatsModel> SeatsList { set; get; }
+    }
+}
